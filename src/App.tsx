78bdc80e98579369f294aee6b636367498e6d9e7
HaileabT <haileabtesfaye11@gmail.com>
2024-10-09 05:01:59 -0400

    j

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   const tg = Telegram.WebApp;
   useEffect(() => {
     tg.ready();
@@ -27,7 +27,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={sendHello}>
-          count is {count}
+          count is sent
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
